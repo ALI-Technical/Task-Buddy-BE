be504b75d4affd2334ad7ef6281229ca30392771
ALI-Technical <ali.aa251104@gmail.com>
2025-02-27 09:18:03 +0500

    changes

--- src/Services/authService.ts
+++ src/Services/authService.ts
@@ -2,7 +2,9 @@ import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import User from "../Models/User";
 
-const JWT_SECRET = process.env.JWT_SECRET || "5as4d4a56465HGHJBASBHJ56458YYGHJHJGHGioasoioi56456";
+const JWT_SECRET =
+  process.env.JWT_SECRET ||
+  "5as4d4a56465HGHJBASBHJ56458YYGHJHJGHGioasoioi56456";
 
 export const registerUser = async (
   email: string,
@@ -25,9 +27,12 @@ export const loginUser = async (email: string, password: string) => {
   const isMatch = await bcrypt.compare(password, user.password);
   if (!isMatch) throw new Error("Invalid credentials");
 
+  // Create a new object without the password field
+  const { password: userPassword, ...userWithoutPass } = user.toObject();
+
   const token = jwt.sign({ userId: user._id, role: user.role }, JWT_SECRET, {
-    expiresIn: "1h",
+    expiresIn: "7d",
   });
 
-  return { token, user };
+  return { token, user: userWithoutPass };
 };
